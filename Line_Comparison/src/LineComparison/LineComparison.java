337b281da9dc0a1a9e43b48252a0dfd002e2c05e
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-09 16:34:01 +0530

    Added code to check whether 2 lines are equal or not

--- Line_Comparison/src/LineComparison/LineComparison.java
+++ Line_Comparison/src/LineComparison/LineComparison.java
@@ -1,22 +1,39 @@
+package LineComparison;
+
 import java.util.Scanner;
 
 public class LineComparison {
 
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		int x1, x2, y1, y2;
-		double lengthOfLine;
+		int x1, x2, x3, x4, y1, y2, y3, y4;
+		double lengthOfLine1, lengthOfLine2;
 		System.out.println("Welcome to Line Comparison Computation Program");
-		System.out.println("Enter the first point");
+		System.out.println("Enter the first point of line1");
 		x1 = sc.nextInt();
-		System.out.println("Enter the second point");
+		System.out.println("Enter the second point of line1");
 		x2 = sc.nextInt();
-		System.out.println("Enter the third point");
+		System.out.println("Enter the third point of line1");
 		y1 = sc.nextInt();
-		System.out.println("Enter the fourth point");
+		System.out.println("Enter the fourth number of line1");
 		y2 = sc.nextInt();
-		lengthOfLine = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-		System.out.println(lengthOfLine);
+		System.out.println("Enter the first point of line2");
+		x3 = sc.nextInt();
+		System.out.println("Enter the second point of line2");
+		x4 = sc.nextInt();
+		System.out.println("Enter the third point of line2");
+		y3 = sc.nextInt();
+		System.out.println("Enter the fourth point of line2");
+		y4 = sc.nextInt();
 		sc.close();
+		lengthOfLine1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		lengthOfLine2 = Math.sqrt(Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
+		String s1 = String.valueOf(lengthOfLine1);
+		String s2 = String.valueOf(lengthOfLine2);
+		if (s2.equals(s1)) {
+			System.out.println("Lines are equal");
+		} else
+			System.out.println("Lines are not equal");
+
 	}
 }
