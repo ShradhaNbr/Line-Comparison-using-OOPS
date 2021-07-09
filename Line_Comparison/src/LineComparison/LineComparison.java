450f1f202ffab0a364c06776ba194097ae2a3e0b
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-09 16:29:24 +0530

    Added code to check length of line

--- Line_Comparison/src/LineComparison/LineComparison.java
+++ Line_Comparison/src/LineComparison/LineComparison.java
@@ -1,27 +1,22 @@
-package LineComparison;
-
 import java.util.Scanner;
 
 public class LineComparison {
-	public static double findLength(int x1, int x2, int y1, int y2) {
-		double lengthOfLine = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y1-y2),2));
-		System.out.println("Length of line is " + lengthOfLine);
-		return lengthOfLine;
-		
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int x1, x2, y1, y2;
+		double lengthOfLine;
+		System.out.println("Welcome to Line Comparison Computation Program");
+		System.out.println("Enter the first point");
+		x1 = sc.nextInt();
+		System.out.println("Enter the second point");
+		x2 = sc.nextInt();
+		System.out.println("Enter the third point");
+		y1 = sc.nextInt();
+		System.out.println("Enter the fourth point");
+		y2 = sc.nextInt();
+		lengthOfLine = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		System.out.println(lengthOfLine);
+		sc.close();
 	}
-public static void main(String[] args) {
-	int x1, x2, y1, y2;
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter the value of x1 oordinate");
-	x1=sc.nextInt();
-	System.out.println("Enter the value of x2 coordinate");
-	x2=sc.nextInt();
-	System.out.println("Enter the value of y1 coordinate");
-	y1=sc.nextInt();
-	System.out.println("Enter the value of y2 coordinate");
-	y2=sc.nextInt();
-	LineComparison lineCompare = new LineComparison();
-	lineCompare.findLength(x1, x2,y1,y2);
-	sc.close();
-}
 }
